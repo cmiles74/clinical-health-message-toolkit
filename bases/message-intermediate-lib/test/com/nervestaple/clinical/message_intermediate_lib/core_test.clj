d56555773d9c6a838d20b26a24e889a6d8459610
Christopher Miles <twitch@nervestaple.com>
2026-08-30 20:04:34 -0400

    :white_check_mark: Updated tests

--- bases/message-intermediate-lib/test/com/nervestaple/clinical/message_intermediate_lib/core_test.clj
+++ bases/message-intermediate-lib/test/com/nervestaple/clinical/message_intermediate_lib/core_test.clj
@@ -1,6 +1,6 @@
 (ns com.nervestaple.clinical.message-intermediate-lib.core-test
   (:require [clojure.test :as test :refer :all]
-            [com.nervestaple.clinical.message-intermediate-lib.core :as core]))
+            [com.nervestaple.clinical.message-intermediate-lib.api :as core]))
 
 (deftest dummy-test
   (is (= 1 1)))
