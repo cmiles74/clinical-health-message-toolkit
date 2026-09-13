0b2b55a07088ffcb037f69db2dc517dc1aa7c0c1
Christopher Miles <twitch@nervestaple.com>
2026-09-13 14:59:13 -0400

    :recycle: More fussing with pom, release v0.14

--- build.clj
+++ build.clj
@@ -117,7 +117,7 @@
                                :name "git"
                                :url "https://github.com/cmiles74/clinical-health-message-toolkit"
                                :connection "scm:git:git://github.com/cmiles74/clinical-health-message-toolkit.git"
-                               :developerConnection "git@github.com:cmiles74/clinical-health-message-toolkit.git"}
+                               :developerConnection "scm:git:ssh://git@github.com:cmiles74/clinical-health-message-toolkit.git"}
                          :src-pom "partial_pom.xml"
                          :version current-version
                          :pom-data licenses})]
